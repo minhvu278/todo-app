13002ec70acba4fab5689080b55720b21284afd8
vu <dominhvu278@gmail.com>
2021-09-16 11:05:55 +0700

    todo-app

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+    <!-- Input -->
+    <div class="d-flex">
+      <input
+        v-model="task"
+        type="text"
+        placeholder="Enter task"
+        class="form-control"
+      />
+      <button @click="submitTask" class="btn btn-warning rounded-0">
+        SUBMIT
+      </button>
+    </div>
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ finished: task.status === 'finished' }">{{
+              task.name
+            }}</span>
+          </td>
+          <td>
+            <span @click="changeStatus(index)" class="pointer">
+              {{ firstCharUpper(task.status) }}
+            </span>
+          </td>
+          <td>
+            <div class="text-center" @click="editTask(index)">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      availableStatuses: ["to-do", "in-progress", "finished"],
+      tasks: [],
+    };
+  },
+  methods: {
+    submitTask() {
+      if (this.task.length === 0) return;
+
+      if (this.editedTask === null) {
+        this.tasks.push({
+          name: this.task,
+          status: "to-do",
+        });
+      } else {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      }
+      this.task = "";
+      this.saveToDatabase();
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    changeStatus(index) {
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+    },
+    firstCharUpper(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    saveToDatabase() {
+      const str = JSON.stringify(this.tasks);
+      localStorage.setItem("todo-database", str);
+    },
+  },
+  mounted() {
+    const rawValue = localStorage.getItem("todo-database");
+    console.log("raw: ", rawValue);
+    if (rawValue) {
+      const data = JSON.parse(rawValue);
+      this.tasks = data;
+    }
+  },
+};
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+.finished {
+  text-decoration: line-through;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/home/vu/code/minhvu278.github.io/projects/todo-app/' // Thay tên repository của các bạn vào đây nhé
+      : '/'
+  }
